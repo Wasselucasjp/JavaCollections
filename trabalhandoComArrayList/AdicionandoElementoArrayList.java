7fa980bb552be098a88e187badb8b9a3fbd257a4
Wasse Lucas <wasselucas.silva@gmail.com>
2023-10-20 15:59:41 -0300

    trabalhando com array list

---
+++ trabalhandoComArrayList/AdicionandoElementoArrayList.java
@@ -0,0 +1,24 @@
+package br.com.alura.trabalhandoComArrayList;
+
+import java.util.ArrayList;
+
+public class AdicionandoElementoArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	       String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+	        String curso2 = "Apache Camel";
+	        String curso3 = "Certificacao Java SE 8 Programmer I";
+
+	        ArrayList<String> cursos = new ArrayList<>();
+	        cursos.add(curso1);
+	        cursos.add(curso2);
+	        cursos.add(curso3);  
+	        
+	        System.out.println(cursos);
+	        
+		
+	}
+
+}
